96674ddd791d04b5daaaa74115d85011dbbb47a5
Odinokun <odinokun@gmail.com>
2022-03-14 10:16:59 +0100

    add title hover effects on title

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
-  './node_modules/svg4everybody/dist/svg4everybody.min.js'
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
+  './node_modules/gsap/dist/gsap.js'
 ];
